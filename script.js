6d0a26729ddfdd34cdc6d3607f13c311c5d46879
Joao Carlos <sjoao5083@gmail.com>
2022-03-30 17:58:49 -0300

    calculadora imc

---
+++ script.js
@@ -0,0 +1,38 @@
+
+let calcular = document.getElementById('calcular');
+
+
+function imc(){
+    let nome = document.getElementById('nome').value;
+    let altura = document.getElementById('altura').value;
+    let peso = document.getElementById('peso').value;
+    let resultado = document.getElementById('resultado');
+
+    if(nome !== '' && altura !=='' && peso !==''){
+        
+        let valorIMC = (peso / (altura * altura)).toFixed(1);
+        let classificacao = '';
+
+        if(valorIMC < 18.5){
+            classificacao = 'abaixo do peso.'
+        } else if(valorIMC < 25){
+            classificacao = 'com o peso ideal.'
+        } else if(valorIMC < 30){
+            classificacao = 'acima do peso.'
+        } else if (valorIMC < 35){
+            classificacao = 'com obesidade grau I'
+        } else if (valorIMC < 40){
+            classificacao = 'com obesidade grau II'
+        } else {
+            classificacao = 'com obesidade grau III. Cuidado'
+        }
+
+        resultado.innerHTML = `${nome} seu IMC é ${valorIMC} e você está ${classificacao}`
+
+    } else {
+        resultado.innerText = 'erro';
+    }
+
+}
+
+calcular.addEventListener('click', imc)
